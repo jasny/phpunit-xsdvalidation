48cc4ca7a80407a4a17ad37f718f19facedf4fc9
aWuttig <andr.wuttig@gmail.com>
2017-10-09 11:32:35 +0200

    * [TASK] Adds php 6.4 as dep * [TASK] Fixes namespace for phpunit contstraint class

--- src/Jasny/PHPUnit/Constraint/XSDValidation.php
+++ src/Jasny/PHPUnit/Constraint/XSDValidation.php
@@ -1,48 +1,50 @@
 <?php
 namespace Jasny\PHPUnit\Constraint;
 
+use PHPUnit\Framework\Constraint\Constraint;
+
 /**
  * PHPUnit constraint to validate against XSD
  */
-class XSDValidation extends \PHPUnit_Framework_Constraint
+class XSDValidation extends Constraint
 {
     /**
      * XSD schema filename or source
      * @var string
      */
     protected $schema;
-    
+
     /**
      * LibXML errors
      * @var array
      */
     protected $errors = [];
-    
-    
+
+
     /**
      * Class constructor
-     * 
+     *
      * @param string $schema   filename or source
      */
     public function __construct($schema)
     {
         if (method_exists(get_parent_class(), '__construct')) parent::__construct();
-        
+
         $this->schema = $schema;
         if (!$this->schemaIsXml() && !file_exists($this->schema))
             throw new \Exception("Schema {$this->schema} doesn't exist");
     }
-    
+
     /**
      * Check if schema contains a '<' character.
-     * 
+     *
      * @return string
      */
     protected function schemaIsXml()
     {
         return strpos($this->schema, '<') !== false;
     }
-    
+
     /**
      * Evaluates the constraint for parameter $other. Returns true if the
      * constraint is met, false otherwise.
@@ -55,7 +57,7 @@ class XSDValidation extends \PHPUnit_Framework_Constraint
     protected function matches($other)
     {
         libxml_use_internal_errors(true);
-        
+
         if ($other instanceof \SimpleXMLElement) {
             $dom = new \DOMDocument('1.0');
             $dom->appendChild($dom->importNode(dom_import_simplexml($other), true));
@@ -67,10 +69,10 @@ class XSDValidation extends \PHPUnit_Framework_Constraint
 
         $ret = $this->schemaIsXml() ? $dom->schemaValidateSource($this->schema) : $dom->schemaValidate($this->schema);
         if (!$ret) $this->errors = libxml_get_errors();
-        
+
         return $ret;
     }
-    
+
     /**
      * Return the XML errors as additional failure description
      *
@@ -80,14 +82,14 @@ class XSDValidation extends \PHPUnit_Framework_Constraint
     protected function additionalFailureDescription($other)
     {
         $desc = '';
-        
+
         foreach ($this->errors as $error) {
             $desc .= " - " . trim($error->message) . "\n";
         }
-        
+
         return $desc;
     }
-    
+
     /**
      * Returns the description of the failure
      *
@@ -109,10 +111,10 @@ class XSDValidation extends \PHPUnit_Framework_Constraint
         } else {
             $xml = $other;
         }
-        
+
         return $xml . ' ' . $this->toString();
     }
-    
+
     /**
      * Returns a string representation of the constraint.
      *
